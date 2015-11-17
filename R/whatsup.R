f05d2da0c66c5e5f6bc596bacc5b2f973e534d83
Aaron Robotham <aaron.robotham@uwa.edu.au>
2015-11-17 14:02:24 +0800

    Various fixes moved across from temporary computer.

--- R/whatsup.R
+++ R/whatsup.R
@@ -135,9 +135,24 @@ dayup=function(RA="12:30:16", Dec="-30:13:15", Target='user', Date='get', Time=c
   return=out
 }
 
+dayupmulti=function(Targets=cbind(names=c('G02', 'G09', 'G12', 'G15', 'G23'), RA=c("02:00:00", "09:00:00", "12:00:00", "14:30:00", "23:00:00"), Dec=c("-05:00:00", "01:00:00", "-01:00:00", "01:00:00", "-32:30:00")),...){
+  out=list()
+  for(i in 1:length(Targets[,1])){
+    out=c(out,list(dayup(RA=Targets[i,"RA"], Dec=Targets[i,"Dec"],...)))
+  }
+  out=c(out,list(names=Targets[,'names']))
+  class(out)="dayupmulti"
+  return=out
+}
+
 plot.dayup=function(x,...){
   if(class(x)!='dayup'){stop('Object must be of type dayup')}
   plotdayup(x,...)
+}
+
+plot.dayupmulti=function(x,...){
+  if(class(x)!='dayupmulti'){stop('Object must be of type dayupmulti')}
+  plotdayupmulti(x,...)
 }
 
 plotdayup=function(obsdata, ytype='Alt',moonphase=TRUE){
@@ -187,8 +202,7 @@ plotdayup=function(obsdata, ytype='Alt',moonphase=TRUE){
   ymoon[moonselect]=NA
   xsun[sunselect]=NA
   ysun[sunselect]=NA
-  magplot(xtarget, ytarget, xaxt='n', yaxt='n', type='n', xlab='Local Time / Hours', ylab='Az / deg', ylim=c(0,380), col='blue')
-  lines(xtarget, ytarget, col='blue')
+  magplot(xtarget, ytarget, xaxt='n', yaxt='n', type='l', xlab='Local Time / Hours', ylab='Az / deg', ylim=c(0,380), col='blue')
   lines(xmoon, ymoon, col=mooncol)
   lines(xsun, ysun, col='orange')
   axis.POSIXct(1, obsdata$at, obsdata$at, format = '%H', tcl=0.5, mgp=c(2,0.5,0))
@@ -202,6 +216,88 @@ plotdayup=function(obsdata, ytype='Alt',moonphase=TRUE){
   abline(h=c(0,30,60,90,120,150,180,210,240,270,300,330,360), lty=c(1,3,3,2,3,3,2,3,3,2,3,3,1), col='grey')
   legend('bottomright', legend=c('Target',' Sun', 'Moon'), col=c('blue','orange',mooncol),lty=1)
 
+}
+
+plotdayupmulti=function(obsdata, ytype='Alt',moonphase=TRUE){
+  layout(rbind(1,2))
+  par(mar=c(0,0,0,0))
+  par(oma=c(3.1,3.1,3.1,2.1))
+  if(moonphase){mooncol=hsv(h=0, s=0, v=1-obsdata[[1]]$moonphase)}else{mooncol='darkgrey'}
+  if(ytype=='Alt'){
+    magplot(obsdata[[1]]$obs$LTPOSIX, obsdata[[1]]$obs$Alt, xaxt='n', type='l', ylim=c(-10,90), xlab='', ylab='Alt / deg', tcl=0.5, mgp=c(2,0.5,0), col='blue', prettybase=30)
+    for(i in 2:(length(obsdata)-1)){lines(obsdata[[i]]$obs$LTPOSIX, obsdata[[i]]$obs$Alt, col='blue')}
+    lines(obsdata[[1]]$obs$LTPOSIX, obsdata[[1]]$obs$AltMoon, col=mooncol)
+    lines(obsdata[[1]]$obs$LTPOSIX, obsdata[[1]]$obs$AltSun, col='orange')
+    axis.POSIXct(1, obsdata[[1]]$at, obsdata[[1]]$at, format = '%H', tcl=0.5, mgp=c(2,0.5,0))
+    abline(v=obsdata[[1]]$dayline+c(-24,-12,0,12,24)*3600, lty=2, col='grey')
+    abline(v=obsdata[[1]]$dayline+c(-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23)*3600, lty=3, col='grey')
+    abline(v=obsdata[[1]]$rise, lty=2, col='orange')
+    abline(v=obsdata[[1]]$set, lty=2, col='orange')
+    abline(h=c(-10,0,10,20,30,40,50,60,70,80,90), lty=c(3,1,3,3,2,3,3,2,3,3,1), col='grey')
+    for(i in 1:(length(obsdata)-1)){
+      abline(v=obsdata[[i]]$obs$LTPOSIX[which.max(obsdata[[i]]$obs$Alt)], lty=3, col='blue')
+      text(obsdata[[i]]$obs$LTPOSIX[which.max(obsdata[[i]]$obs$Alt)], obsdata[[i]]$obs$Alt[which.max(obsdata[[i]]$obs$Alt)],obsdata$names[i])
+    }
+  }
+  if(ytype=='AM'){
+    magplot(obsdata[[1]]$obs$LTPOSIX, obsdata[[1]]$obs$AirMass, xaxt='n', type='l', ylim=c(3,1), xlab='', ylab='Air Mass', tcl=0.5, mgp=c(2,0.5,0),col='blue')
+    for(i in 2:length(obsdata)){lines(obsdata[[i]]$obs$LTPOSIX, obsdata[[i]]$obs$AirMass, col='blue')}
+    lines(obsdata[[1]]$obs$LTPOSIX, obsdata[[1]]$obs$AirMassMoon, col=mooncol)
+    lines(obsdata[[1]]$obs$LTPOSIX, obsdata[[1]]$obs$AirMassSun, col='orange')
+    axis.POSIXct(1, obsdata[[1]]$at, obsdata[[1]]$at, format = '%H', tcl=0.5, mgp=c(2,0.5,0))
+    abline(v=obsdata[[1]]$dayline+c(-24,-12,0,12,24)*3600, lty=2, col='grey')
+    abline(v=obsdata[[1]]$dayline+c(-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23)*3600, lty=3, col='grey')
+    abline(v=obsdata[[1]]$rise, lty=2, col='orange')
+    abline(v=obsdata[[1]]$set, lty=2, col='orange')
+    abline(v=obsdata[[1]]$obs$LTPOSIX[which.max(obsdata[[1]]$obs$Alt)], lty=3, col='blue')
+    abline(h=airmass(c(-10,0,10,20,30,40,50,60,70,80,90)), lty=c(3,1,3,3,2,3,3,2,3,3,1), col='grey')
+    for(i in 1:(length(obsdata)-1)){
+      abline(v=obsdata[[i]]$obs$LTPOSIX[which.max(obsdata[[i]]$obs$Alt)], lty=3, col='blue')
+      text(obsdata[[i]]$obs$LTPOSIX[which.max(obsdata[[i]]$obs$Alt)], obsdata[[i]]$obs$AirMass[which.max(obsdata[[i]]$obs$Alt)],obsdata$names)
+    }
+  }
+  
+  title(main=paste('Y', obsdata[[1]]$obs$LT.year[1], 'M', obsdata[[1]]$obs$LT.mon[1], 'D', obsdata[[1]]$obs$LT.mday[1], ', Lon ', round(obsdata[[1]]$Lon), ', Lat ', round(obsdata[[1]]$Lat),', Mphase ',round(obsdata[[1]]$moonphase,2),sep=''), outer=TRUE)
+  
+  select=which(abs(diff(obsdata[[1]]$obs$Az))>200)
+  moonselect=which(abs(diff(obsdata[[1]]$obs$AzMoon))>200)
+  sunselect=which(abs(diff(obsdata[[1]]$obs$AzSun))>200)
+  xtarget=obsdata[[1]]$obs$LTPOSIX
+  ytarget=obsdata[[1]]$obs$Az
+  xmoon=obsdata[[1]]$obs$LTPOSIX
+  ymoon=obsdata[[1]]$obs$AzMoon
+  xsun=obsdata[[1]]$obs$LTPOSIX
+  ysun=obsdata[[1]]$obs$AzSun
+  xtarget[select]=NA
+  ytarget[select]=NA
+  xmoon[moonselect]=NA
+  ymoon[moonselect]=NA
+  xsun[sunselect]=NA
+  ysun[sunselect]=NA
+  magplot(xtarget, ytarget, xaxt='n', yaxt='n', type='l', xlab='Local Time / Hours', ylab='Az / deg', ylim=c(0,380), col='blue')
+  for(i in 2:(length(obsdata)-1)){
+    select=which(abs(diff(obsdata[[i]]$obs$Az))>200)
+    xtarget=obsdata[[i]]$obs$LTPOSIX
+    ytarget=obsdata[[i]]$obs$Az
+    xtarget[select]=NA
+    ytarget[select]=NA
+    lines(xtarget, ytarget, col='blue')
+  }
+  lines(xmoon, ymoon, col=mooncol)
+  lines(xsun, ysun, col='orange')
+  axis.POSIXct(1, obsdata[[1]]$at, obsdata[[1]]$at, format = '%H', tcl=0.5, mgp=c(2,0.5,0))
+  magaxis(2,prettybase=90,labels=FALSE)
+  axis(2, at=c(0,90,180,270,360), labels=c('0N','90E','180S','270W','360N'), tick=FALSE, tcl=0.5, mgp=c(2,0.5,0))
+  abline(v=obsdata[[1]]$dayline+c(-24,-12,0,12,24)*3600, lty=2, col='grey')
+  abline(v=obsdata[[1]]$dayline+c(-23,-22,-21,-20,-19,-18,-17,-16,-15,-14,-13,-11,-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,1,2,3,4,5,6,7,8,9,10,11,13,14,15,16,17,18,19,20,21,22,23)*3600, lty=3, col='grey')
+  abline(v=obsdata[[1]]$rise, lty=2, col='orange')
+  abline(v=obsdata[[1]]$set, lty=2, col='orange')
+  abline(h=c(0,30,60,90,120,150,180,210,240,270,300,330,360), lty=c(1,3,3,2,3,3,2,3,3,2,3,3,1), col='grey')
+  for(i in 1:(length(obsdata)-1)){
+    abline(v=obsdata[[i]]$obs$LTPOSIX[which.max(obsdata[[i]]$obs$Alt)], lty=3, col='blue')
+  }
+  legend('bottomright', legend=c('Target',' Sun', 'Moon'), col=c('blue','orange',mooncol),lty=1)
+  
 }
 
 nowup=function(Ncut=20, Azlim=c(0,360), Altlim=c(60,90), Date='get', Time='get', Lon=115.8178, Lat=-31.97333, Loc='user', UTCdiff='get', Altitude=10, Pressure=1000, Temp=20, select=c('G','S')){
